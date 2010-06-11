add29386686e399c88ba7b6537918ba291788e64
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-11 09:16:53 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_backdate_blocker do
+  name 'Redmine Backdate Blocker plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
