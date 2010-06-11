1fcd7e5dc23e4c21c0533ec6e012a7b15e66c5c0
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-11 15:35:16 -0700

    [#4159] Refactor TimeEntry#check_for_backdated_spent_on

--- lib/redmine_backdate_blocker/patches/time_entry_patch.rb
+++ lib/redmine_backdate_blocker/patches/time_entry_patch.rb
@@ -22,13 +22,10 @@ module RedmineBackdateBlocker
       module InstanceMethods
         def check_for_backdated_spent_on
           return if Setting.plugin_redmine_backdate_blocker['days'].blank?
+          return if self.class.backdate_blocker_days.days.ago.beginning_of_day <= spent_on.beginning_of_day
 
-          backdated_days = Setting.plugin_redmine_backdate_blocker['days'].to_i
-
-          return if backdated_days.to_i.days.ago.beginning_of_day <= spent_on.beginning_of_day
-
-          unless User.current.allowed_to?(:backdate_time, project)
-            errors.add(:spent_on, l(:backdate_blocker_text_must_be_within_days, :days => backdated_days))
+          unless allowed_to_backdate?
+            errors.add(:spent_on, l(:backdate_blocker_text_must_be_within_days, :days => self.class.backdate_blocker_days))
           end
         end
 
